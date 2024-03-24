c8d9487d4361e580889820964fb4997bdd1be861
dante.barbosa@avonale.com.br <danteveloso2019@gmail.com>
2024-03-24 12:38:06 -0300

    feat(Initial): Initial Commit, estructuring the project

---
+++ KonsiCredit/Presentation/Presentation.KonsiCredit/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using Application.KonsiCredit.AuthViewModels;
+using Domain.KonsiCredit;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Services.KonsiCredit.AuthAppService;
+
+namespace Presentation.KonsiCredit.Controllers;
+
+[Route("konsiCredit/Auth/v1")]
+public class AuthController : ControllerBase
+{
+    
+    private readonly ILogger<User> _logger;
+    private readonly IConfiguration _configuration;
+    private readonly IAuthAppService _authAppService;
+
+    public AuthController(ILogger<User> logger, IConfiguration configuration,
+        IAuthAppService authAppService)
+    {
+        _configuration = configuration;
+        _logger = logger;
+        _authAppService = authAppService;
+    }
+    
+    [AllowAnonymous]
+    [Route("/userToken")]
+    [HttpGet]
+    public async Task<IActionResult> GetUserToken(UserViewModel userViewModel)
+    {
+        var response = await _authAppService.GetUserToken(userViewModel.Username, userViewModel.Password);
+        return Ok(response);
+    }
+    
+}
---
+++ KonsiCredit/Presentation/Presentation.KonsiCredit/Controllers/BenefitsController.cs
@@ -0,0 +1,33 @@
+﻿using Domain.KonsiCredit;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Services.KonsiCredit.BenefitsAppService;
+
+namespace Presentation.KonsiCredit.Controllers;
+
+[Route("konsiCredit/v1")]
+public class BenefitsController: ControllerBase
+{
+    
+    private readonly ILogger<User> _logger;
+    private readonly IConfiguration _configuration;
+    private readonly IBenefitsAppService _benefitsAppService;
+
+    public BenefitsController(ILogger<User> logger, IConfiguration configuration,
+        IBenefitsAppService benefitsAppService)
+    {
+        _configuration = configuration;
+        _logger = logger;
+        _benefitsAppService = benefitsAppService;
+    }
+    
+    [HttpGet]
+    [Route("consulta-beneficios")]
+    public async Task<IActionResult> GetByCpf([FromQuery] string cpf)
+    {
+        var token = Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+        var response = await _benefitsAppService.GetUserBenefits(cpf, token);
+        return Ok(response);
+    }
+
+}
